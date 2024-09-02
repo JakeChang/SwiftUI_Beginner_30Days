3325feeb1e035ca10012a1aa131dfdb6ac55acbe
Jake Chang <>
2024-09-02 12:23:33 +0800

    - Add Day1

---
+++ Day1/Day1/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Day1
+//
+//  Created by Jake Chang on 2024/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .font(.system(size: 30, weight: .bold))
+            .foregroundStyle(.blue)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day1/Day1/Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  Day1App.swift
+//  Day1
+//
+//  Created by Jake Chang on 2024/9/2.
+//
+
+import SwiftUI
+
+@main
+struct Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
